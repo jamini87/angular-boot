5974cf0abfb9ae4fbc26934218e66eaa1ed6c617
Jafar Amini <jafar.amini99@gmail.com>
2019-03-03 17:14:22 +0330

    Added @angular-boot/validation library

---
+++ projects/angular-boot/validation/src/lib/validation.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ValidationComponent } from './validation.component';
+
+@NgModule({
+  declarations: [ValidationComponent],
+  imports: [
+  ],
+  exports: [ValidationComponent]
+})
+export class ValidationModule { }
---
+++ projects/angular-boot/validation/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of validation
+ */
+
+export * from './lib/validation.service';
+export * from './lib/validation.component';
+export * from './lib/validation.module';
