599cf778ca6740b41b7fbb597bc38fdc61aac9c9
Jafar Amini <jafar.amini99@gmail.com>
2019-03-11 11:58:04 +0330

    version 0.3.8

--- projects/angular-boot/util/src/lib/nb-helper/helper/signals/list-helper.ts
+++ projects/angular-boot/util/src/lib/nb-helper/helper/signals/list-helper.ts
@@ -1,13 +1,15 @@
-import {Paging, SpringSorting} from '@angular-boot/util';
+import {Paging} from '../query';
+import {SpringSorting} from '../helper';
 
 export class ListHelper {
   paging: Paging;
   sortings: SpringSorting[];
   term: string;
-
-  constructor(init: { paging?: Paging, sortings?: SpringSorting[], term?: string }) {
+  options: any;
+  constructor(init: { paging?: Paging, sortings?: SpringSorting[], term?: string , options?: any}) {
     this.paging = init.paging || new Paging();
     this.sortings = init.sortings || [];
     this.term = init.term || '';
+    this.options = init.options || {};
   }
 }
