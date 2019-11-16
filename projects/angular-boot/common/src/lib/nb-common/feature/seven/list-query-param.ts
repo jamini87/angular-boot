43e5c1427a85673451e691c2006d469331476205
Jafar Amini <jafar.amini99@gmail.com>
2019-11-16 13:40:18 +0330

    term added to ListQueryParam

--- projects/angular-boot/common/src/lib/nb-common/feature/seven/list-query-param.ts
+++ projects/angular-boot/common/src/lib/nb-common/feature/seven/list-query-param.ts
@@ -3,9 +3,10 @@ import {Paging} from '@angular-boot/util';
 export class ListQueryParam {
   paging: Paging;
   itemId: string;
-
+  term: string;
   constructor() {
     this.paging = new Paging();
     this.itemId = null;
+    this.term = null;
   }
 }
