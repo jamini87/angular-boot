df625a3486c5faa1b7c02e29ad840f0c113010f9
Jafar Amini <jafar.amini99@gmail.com>
2020-01-28 17:57:51 +0330

    PickModel constructor create new instance for its data

--- projects/angular-boot/util/src/lib/nb-helper/helper/helper/pick-model.ts
+++ projects/angular-boot/util/src/lib/nb-helper/helper/helper/pick-model.ts
@@ -5,9 +5,9 @@ export class PickModel<T = any> {
   index?: number;
   data?: T;
 
-  constructor(init: PickModel = {}) {
+  constructor(init: PickModel = {}, Type: (new () => T)) {
     !isUndefined(init.id) ? this.id = init.id : this.id = null;
     !isUndefined(init.index) ? this.index = init.index : this.index = null;
-    !isUndefined(init.data) ? this.data = init.data : this.data = null;
+    !isUndefined(init.data) ? this.data = init.data : this.data = new Type;
   }
 }
