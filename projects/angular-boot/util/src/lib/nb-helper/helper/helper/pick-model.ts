889bd46ea25b9684ba2f4ebe5272da6ed07fd759
Jafar Amini <jafar.amini99@gmail.com>
2020-01-30 11:30:01 +0330

    Capital of asc, desc added to sorting-icon component

--- projects/angular-boot/util/src/lib/nb-helper/helper/helper/pick-model.ts
+++ projects/angular-boot/util/src/lib/nb-helper/helper/helper/pick-model.ts
@@ -5,9 +5,25 @@ export class PickModel<T = any> {
   index?: number;
   data?: T;
 
-  constructor(init: PickModel = {}, Type: (new () => T)) {
+  constructor(Type?: (new () => T), init: PickModel = {}) {
     !isUndefined(init.id) ? this.id = init.id : this.id = null;
     !isUndefined(init.index) ? this.index = init.index : this.index = null;
-    !isUndefined(init.data) ? this.data = init.data : this.data = new Type;
+    !isUndefined(init.data) ? this.data = init.data : Object.assign(this.data, {});
   }
+
+
+  // constructor(Type?: (new () => T), init: PickModel = {}) {
+  //   !isUndefined(init.id) ? this.id = init.id : this.id = null;
+  //   !isUndefined(init.index) ? this.index = init.index : this.index = null;
+  //   if (Type) {
+  //     !isUndefined(init.data) ? this.data = init.data : this.data = new Type;
+  //   } else {
+  //     !isUndefined(init.data) ? this.data = init.data : Object.assign(this.data, {});
+  //   }
+  // }
+  // constructor(init: PickModel = {}) {
+  //   !isUndefined(init.id) ? this.id = init.id : this.id = null;
+  //   !isUndefined(init.index) ? this.index = init.index : this.index = null;
+  //   !isUndefined(init.data) ? this.data = init.data : Object.assign(this.data, {});
+  // }
 }
