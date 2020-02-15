8dc82bc17e1f9071fd26cdb12de08a0b6062b3ec
Jafar Amini <jafar.amini99@gmail.com>
2020-02-15 13:53:52 +0330

    date convert bug fixed

--- projects/angular-boot/util/src/lib/nb-util/toolkit/moment.ts
+++ projects/angular-boot/util/src/lib/nb-util/toolkit/moment.ts
@@ -6,96 +6,102 @@
 // import * as moment from 'jalali-moment';
 
 
-import * as momentImported from 'jalali-moment';
-
+import * as jalaliMomentImported from 'jalali-moment';
+import * as momentImported from 'moment';
+const jalaliMoment = jalaliMomentImported;
 const moment = momentImported;
 
 
 import {Common} from './common';
 
 export class Moment {
-
+  // *************************************
+  public static isoToJalaliFormat(date, format: JalaliDateFormats) {
+    return jalaliMoment(moment(date, 'YYYY-MM-DDTHH:mm:ss.SSZ').toString()).format(format);
+  }
 
   public static isoToJDay(date) {
     if (date) {
-      return moment(date).format('jYYYY/jMM/jDD' as JalaliDateFormats);
+      return Moment.isoToJalaliFormat(date, 'jYYYY/jMM/jDD');
     }
   }
 
   public static isoToJMin(date) {
     if (date) {
-      return moment(date).format('HH:mm - jYYYY/jMM/jDD' as JalaliDateFormats);
+      return Moment.isoToJalaliFormat(date, 'HH:mm - jYYYY/jMM/jDD');
     }
   }
 
   public static isoToJSec(date) {
     if (date) {
-      return moment(date).format('HH:mm:ss - jYYYY/jMM/jDD' as JalaliDateFormats);
+      return Moment.isoToJalaliFormat(date, 'HH:mm:ss - jYYYY/jMM/jDD');
     }
   }
-
-
+// *************************************
+  public static isoToGregorianFormat(date, format: GregorianDateFormats) {
+    return moment(date).format(format);
+  }
   public static isoToGDay(date) {
     if (date) {
-      return moment(date).format('YYYY/MM/DD' as GregorianDateFormats);
+      return Moment.isoToGregorianFormat(date, 'YYYY/MM/DD');
     }
   }
 
   public static isoToGMin(date) {
     if (date) {
-      return moment(date).format('HH:mm - YYYY/MM/DD' as GregorianDateFormats);
+      return Moment.isoToGregorianFormat(date, 'HH:mm - YYYY/MM/DD');
     }
   }
 
   public static isoToGSec(date) {
     if (date) {
-      return moment(date).format('HH:mm:ss - YYYY/MM/DD' as GregorianDateFormats);
+      return Moment.isoToGregorianFormat(date, 'HH:mm:ss - YYYY/MM/DD');
     }
   }
-
+// *************************************
 
   public static getJaliliDateFromIsoOrFull(date) {
     if (date) {
-      return moment(date).format('HH:mm - jYYYY/jMM/jDD');
+      return jalaliMoment(date).format('HH:mm - jYYYY/jMM/jDD');
     }
   }
 
   public static getGregorianDateFromIsoOrFull(date) {
     if (date) {
-      return moment(date).format('HH:mm - YYYY/MM/DD');
+      return jalaliMoment(date).format('HH:mm - YYYY/MM/DD');
     }
   }
 
   public static convertJaliliToGregorian(jDate) {
-    return moment(
-      moment(Common.Fa2En(jDate), 'jYYYY/jMM/jDD').toISOString()
+    return jalaliMoment(
+      jalaliMoment(Common.Fa2En(jDate), 'jYYYY/jMM/jDD').toISOString()
     ).format('YYYY/MM/DD');
   }
 
   public static convertJaliliToGregorianWithTime(jDate) {
-    return moment(
-      moment(Common.Fa2En(jDate), 'HH:mm - jYYYY/jMM/jDD').toISOString()
+    return jalaliMoment(
+      jalaliMoment(Common.Fa2En(jDate), 'HH:mm - jYYYY/jMM/jDD').toISOString()
     ).format('HH:mm - YYYY/MM/DD');
   }
 
   public static convertGregorianToJalili(date) {
-    return moment(
-      moment(Common.Fa2En(date), 'YYYY/MM/DD').toISOString()
+    return jalaliMoment(
+      jalaliMoment(Common.Fa2En(date), 'YYYY/MM/DD').toISOString()
     ).format('YYYY/MM/DD');
   }
 
   public static convertGregorianToJaliliWithTime(date) {
-    return moment(
-      moment(Common.Fa2En(date), 'HH:mm - YYYY/MM/DD').toISOString())
+    return jalaliMoment(
+      jalaliMoment(Common.Fa2En(date), 'HH:mm - YYYY/MM/DD').toISOString())
       .format('HH:mm - YYYY/MM/DD');
   }
 
   public static convertJaliliToIsoDate(jDate) {
-    return moment(Common.Fa2En(jDate), 'jYYYY/jMM/jDD').toISOString();
+    return jalaliMoment(Common.Fa2En(jDate), 'jYYYY/jMM/jDD').toISOString();
   }
 
   public static convertGregorianToIsoDate(jDate) {
-    return moment(Common.Fa2En(jDate), 'YYYY/MM/DD').toISOString();
+    return jalaliMoment(Common.Fa2En(jDate), 'YYYY/MM/DD').toISOString();
   }
 
 }
